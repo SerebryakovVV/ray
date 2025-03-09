e8d5889126c89a3f307f109e596e3c7907fa64fd
SerebryakovVV <serebryakov.valentin@gmail.com>
2025-03-09 21:56:16 +0700

    vec

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io::{self, stderr, Stderr, Write as IoWrite};
 use std::path::{Path, PathBuf};
-use std::fmt::Write;
+use std::fmt::Write as FmtWrite;
+use std::thread::sleep;
 
 
 
@@ -32,6 +34,26 @@ impl<'a> Image<'a> {
         todo!();
     }
 
+    fn make_image_one(&self) {
+        println!("P3");
+        println!("{} {}", self.width, self.height);
+        println!("255");
+        for i in 0..self.height {
+            eprintln!("Remaining lines: {}", self.height - i);
+            std::io::stderr().flush().unwrap();
+            for k in 0..self.width {
+                let r = k as f64 / (self.width-1) as f64; 
+                let g = i as f64 / (self.height-1) as f64; 
+                let b = 0.0f64; 
+                let ir = (255.999 * r) as u8;
+                let ig = (255.999 * g) as u8;
+                let ib = (255.999 * b) as u8;
+                println!("{} {} {}", ir, ig, ib);
+            }
+        }
+        eprint!("done");
+    }
+
     fn clear_data(&mut self) {
         self.data.clear();
     }
@@ -39,27 +61,127 @@ impl<'a> Image<'a> {
 }
 
 
+
+
+
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+impl Vec3 {
+    fn neg(&self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z
+        }
+    }
+
+    fn add_vec(&mut self, v: Self) {
+        self.x += v.x;
+        self.y += v.y;
+        self.z += v.z;            // return the struct, maybe
+    }
+
+    fn mul_scal(&mut self, s: f64) {
+        self.x *= s;
+        self.y *= s;
+        self.z *= s;             // return the struct, maybe
+    }
+
+    fn div_scal(&mut self, s: f64) {
+        self.x /= s;
+        self.y /= s;
+        self.z /= s;         // return the struct, maybe
+    }
+
+    fn new_div_scal_vec(&self, s: f64) -> Self {
+        Self {
+            x: self.x / s,
+            y: self.y / s,
+            z: self.z / s
+
+        }
+    }
+
+    fn length_squared(&self) -> f64 {
+        self.x*self.x
+       +self.y*self.y
+       +self.z*self.z
+    }
+
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn print(&self) {
+        println!("{} {} {}", self.x, self.y, self.z);
+    }
+
+    fn new_sum_vec(&self, v: Self) -> Self {
+        Self {
+            x: self.x + v.x,
+            y: self.y + v.y,
+            z: self.z + v.z
+        }
+    }
+
+    fn new_subtruct_vec(&self, v: Self) -> Self {
+        Self {
+            x: self.x - v.x,
+            y: self.y - v.y,
+            z: self.z - v.z
+        }
+    }
+
+    fn new_mul_vec(&self, v: Self) -> Self {
+        Self {
+            x: self.x * v.x,
+            y: self.y * v.y,
+            z: self.z * v.z
+        }
+    }
+
+    fn new_mul_scal_vec(&self, s: f64) -> Self {
+        Self {
+            x: self.x * s,
+            y: self.y * s,
+            z: self.z * s
+        }
+    }
+
+    fn dot(&self, v: Self) -> f64 {
+        self.x * v.x
+       +self.y * v.y
+       +self.z * v.z
+    }
+
+    fn cross(&self, v: Self) -> Self {
+        Self {
+            x: self.y * v.z - self.z * v.y,
+            y: self.z * v.x - self.x * v.z,
+            z: self.x * v.y - self.y * v.x
+        }
+    }
+    
+    fn unit_vec(&self) -> Self {
+        self.new_div_scal_vec(self.length())
+    }
+
+}
+
+
+
+
 fn main() {
-    // let mut img = Image::new(256, 256, "image.ppm");
+    let mut img = Image::new(256, 256, "image.ppm");
+    img.make_image_one();
     // img.prepare_metadata();
     // println!("{}", img.data);
 
-    println!("P3");
-    println!("256 256");
-    println!("255");
-    for i in 0..256 {
-        for k in 0..256 {
-            let r = k as f64 / 255.0f64; 
-            let g = i as f64 / 255.0f64; 
-            let b = 0.0f64; 
-        
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-        
-            println!("{} {} {}", ir, ig, ib);
-        }
-    }
+
 
  
 }
