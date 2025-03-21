7eff61b0f56223e51f2e533c9e3de5dfb9465fd4
SerebryakovVV <serebryakov.valentin@gmail.com>
2025-03-22 04:09:32 +0700

    red circle

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 #include <cmath>
 
 
-
-
-
-
-
-
-
 class vec3 {
     public:
         double e[3];
@@ -19,7 +12,6 @@ class vec3 {
         double x() const {return e[0];}
         double y() const {return e[1];}
         double z() const {return e[2];}
-
 
         vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);} 
         double operator[](int i) const {return e[i];}
@@ -50,7 +42,6 @@ class vec3 {
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
-
 };
 
 
@@ -104,44 +95,107 @@ inline vec3 unit_vector(const vec3& v) {
 
 
 
-
 using color = vec3;
+
 void write_color(std::ostream& out, const color pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
-
     int rbyte = int(255.999 * r);
     int gbyte = int(255.999 * g);
     int bbyte = int(255.999 * b);
-
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 
 
 
+class ray {
+    public:
+        ray() {}
+
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+        const point3& origin() const {return orig;}
+        const vec3& direction() const {return dir;}
+
+        point3 at(double t) const {
+            return orig + t*dir;
+        }
+        
+
+
+    private:
+        point3 orig;
+        vec3 dir;
+};
+
+
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
+
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r)) return color(1,0,0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 
 
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    
+    int image_width = 400;
+
+
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0,0,0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+
+
+
+
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
+    
 
-            auto pixel_color = color(double(i) / (image_width-1), double(j) / (image_height-1), 0);
+            auto pixel_center = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+           
+           
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
-
-            // auto r = double(i) / (image_width-1);
-            // auto g = double(j) / (image_height-1);
-            // auto b = 0.0;
-            // int ir = int(255.999 * r);
-            // int ig = int(255.999 * g);
-            // int ib = int(255.999 * b);
-            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
     std::clog << "\rDone.    \n";
